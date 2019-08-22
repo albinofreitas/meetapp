08cd284d3544f026df7bd1319fb491de01947c07
AlbinoFreitas <albino_freitas@hotmail.com>
2019-08-22 00:12:47 -0300

    Authentication

---
+++ src/app/controllers/AuthController.js
@@ -0,0 +1,46 @@
+import jwt from 'jsonwebtoken';
+import * as Yup from 'yup';
+
+import User from '../models/User';
+
+import authConfig from '../../config/auth';
+
+class AuthController {
+  async store(req, res) {
+    const shcema = Yup.object().shape({
+      email: Yup.string()
+        .email()
+        .required(),
+      password: Yup.string()
+        .min(6)
+        .required(),
+    });
+
+    if (!(await shcema.isValid(req.body))) {
+      return res.status(422).json({ error: 'Validation fails' });
+    }
+
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ where: { email } });
+
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+
+    if (!(await user.checkPassword(password))) {
+      return res.status(422).json({ error: 'Passord does not match' });
+    }
+
+    const { id } = user;
+
+    return res.json({
+      user: user.resource(),
+      token: jwt.sign({ id }, authConfig.secret, {
+        expiresIn: authConfig.expiresIn,
+      }),
+    });
+  }
+}
+
+export default new AuthController();
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
+import AuthController from './app/controllers/AuthController';
+import UserController from './app/controllers/UserController';
+
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ ok: true });
-});
+routes.post('/users', UserController.store);
+routes.post('/auth', AuthController.store);
 
 export default routes;
