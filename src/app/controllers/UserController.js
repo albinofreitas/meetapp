253264f774876b6d55c1f7e017144061e76f48e5
AlbinoFreitas <albino_freitas@hotmail.com>
2019-08-22 00:11:52 -0300

    Create user

---
+++ src/app/controllers/UserController.js
@@ -0,0 +1,33 @@
+import * as Yup from 'yup';
+
+import User from '../models/User';
+
+class UserController {
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      email: Yup.string()
+        .email()
+        .required(),
+      name: Yup.string().required(),
+      password: Yup.string()
+        .min(6)
+        .required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(422).json({ error: 'Validation fails' });
+    }
+
+    const userExists = await User.findOne({ where: { email: req.body.email } });
+
+    if (userExists) {
+      return res.status(422).json({ error: 'User already exists' });
+    }
+
+    const user = await User.create(req.body);
+
+    return res.json({ user: user.resource() });
+  }
+}
+
+export default new UserController();
