add102e05ec5e52dd063d45f917aa4ef5bcea291
AlbinoFreitas <albino_freitas@hotmail.com>
2019-09-02 00:25:42 -0300

    fix env variables

--- src/config/database.js
+++ src/config/database.js
@@ -1,11 +1,11 @@
 require('dotenv').config();
 
 module.exports = {
-  dialect: process.env.DB_DIALECT,
+  dialect: 'postgres',
   host: process.env.DB_HOST,
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
   define: {
     timestamps: true,
     underscored: true,
